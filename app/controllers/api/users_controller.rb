f4cfefb86fcc7ba97e593168ef2b957acd427bd6
App Academy Student <student@appacademy.io>
2019-07-02 22:01:00 -0700

    Individualize error messages for signup

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,7 @@ class Api::UsersController < ApplicationController
       login(@user)
       render "/api/users/#{@user.id}"
     else
-      render json: @user.errors.full_messages, status: 422
+      render json: @user.errors.messages, status: 422
     end
   end
 
--- frontend/bandvamp.jsx
+++ frontend/bandvamp.jsx
@@ -8,13 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const root = document.getElementById('root');
   let preloadedState = undefined;
-  if (window.currentUser){
-    preloadedState = {
-      entities: { users: { [window.currentUser.id]: window.currentUser}},
-      errors: {session: []},
-      session: {id: window.currentUser.id}
-    };
-  }
+  // if (window.currentUser){
+  //   preloadedState = {
+  //     entities: { users: { [window.currentUser.id]: window.currentUser}},
+  //     errors: {session: []},
+  //     session: {id: window.currentUser.id}
+  //   };
+  // }
   const store = configureStore(preloadedState);
   window.signup = APIUtil.signup;
   window.getState = store.getState;
