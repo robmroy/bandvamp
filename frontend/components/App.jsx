2b681fa107c4291ddc1c65bd4588b146735f6a50
App Academy Student <student@appacademy.io>
2019-07-03 13:25:28 -0700

    Let user select to signup as artist or fan

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,6 +3,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import LogInFormContainer from './session_form/login_form_container';
 import FanSignupContainer from './session_form/fan_signup_container';
 import BandSignupContainer from './session_form/band_signup_container';
+import SelectSignup from './session_form/select_signup';
 import AuthNavContainer from './auth_nav_container';
 import {
   Route,
@@ -16,7 +17,9 @@ const App = () => (
   <div>
     <AuthNavContainer path="/" />
     <AuthRoute exact path="/login" component={LogInFormContainer} />
-    <AuthRoute exact path="/signup" component={BandSignupContainer} />
+    <AuthRoute exact path="/signup" component={SelectSignup} />
+    <AuthRoute exact path="/signup/band" component={BandSignupContainer} />
+    <AuthRoute exact path="/signup/fan" component={FanSignupContainer} />
   </div >
 )
 
