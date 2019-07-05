2c8f7edcbcf13b5e3b011ddef2223b076f069883
robmroy <robmyroy@gmail.com>
2019-07-05 16:51:53 -0700

    Place a (non-functional) search bar

--- frontend/components/header_nav.jsx
+++ frontend/components/header_nav.jsx
@@ -10,14 +10,20 @@ if (props.user_id === null){
   return (
     <div className="header-nav">
       {logoElement}
+      <div className="header-nav-right">
+        <div className="search-bar-wrapper">
+        <input className="search-bar" placeholder="Search music"/>
+        </div>
       <div className="header-links">
+          <div className="black-link-container"> <div className="pad15"> </div>   
       <Link to="/signup" 
       className="black-link">sign up</Link> &nbsp; &nbsp;
     <Link to="/login" className="black-link">log in</Link> &nbsp; &nbsp;
-   
-    <button className="demo-button" onClick={() => props.demo()}>Demo login</button>
+              </div>
+          <button className="demo-button" 
+            onClick={() => props.demo()}>Demo login</button>
       </div>
-     
+      </div>
     </div>
   )
 }
