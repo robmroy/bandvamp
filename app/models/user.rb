0c9e640ad49463738344010c66ee3677b16b0066
robmroy <robmyroy@gmail.com>
2019-07-09 21:35:54 -0700

    Put the validate back into user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :username, :email, presence: true, uniqueness: true
   validates :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6 }, allow_nil: true
-
+  validate :require_photos
   after_initialize :ensure_session_token
 
 
