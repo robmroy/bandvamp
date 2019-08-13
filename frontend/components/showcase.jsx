5ad4bacab978f6b6194a7c6358d9d1b92015343b
robmroy <robmyroy@gmail.com>
2019-08-13 15:38:25 -0700

    Solve brighten hover issue

--- frontend/components/showcase.jsx
+++ frontend/components/showcase.jsx
@@ -4,9 +4,24 @@ import lunasol from '../assets/images/luna_y_sol.png';
 class Showcase extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {brightness: [null, '','','']};
   }
 
+  brighten(i){
+    let brightness = this.state.brightness;
+    brightness[i] = '-bright';
+    return () => this.setState({brightness});
+  }
+  darken(i){
+    let brightness = this.state.brightness;
+    brightness[i] = '';
+    return () => this.setState({brightness});
+  }
   render(){
+    
+   
+    const [brighten, darken]=[this.brighten.bind(this), this.darken.bind(this)];
+
     return 	(
     <div className = 'showcase-pics-container'>
     <div className='showcase-large' >
@@ -23,20 +38,47 @@ class Showcase extends React.Component {
        </div>
        <div className = 'small-pics-container'>
          <div className = 'small-pic-item'>
-         <div className = 'small-pic' style={{"background-image": 'url('+window.sidepic1+')'}}>
+         <div className = {'small-pic'+this.state.brightness[1]} style={{"background-image": 'url('+window.sidepic1+')'}}
+         onMouseEnter={() => {brighten(1)(); }}
+         onMouseLeave={() => darken(1)()}>
            </div>
-           <div className = 'small-pic-text'>High Scores: Ben Prunty’s Moody, Trance-Inspired “Photographs” OST</div>
+           <div className = 'small-pic-text'
+           onMouseEnter={() => {brighten(1)()}} onMouseLeave={() => darken(1)()}>High Scores: Ben Prunty’s Moody, Trance-Inspired “Photographs” OST</div>
          </div>
+
          <div className = 'small-pic-item'>
+         <div className = {'small-pic'+this.state.brightness[2]} style={{"background-image": 'url('+window.sidepic2+')'}}
+         onMouseEnter={() => {brighten(2)(); }}
+         onMouseLeave={() => darken(2)()}>
+           </div>
+           <div className = 'small-pic-text'
+           onMouseEnter={() => {brighten(2)()}}
+           onMouseLeave={() => darken(2)()}>Gabe 'Nandez Finds a Home in Hip-Hop</div>
+         </div>
+
+         <div className = 'small-pic-item'>
+         <div className = {'small-pic'+this.state.brightness[3]} style={{"background-image": 'url('+window.sidepic3+')'}}
+         onMouseEnter={() => {brighten(3)(); }}
+         onMouseLeave={() => darken(3)()}>
+           </div>
+           <div className = 'small-pic-text'
+           onMouseEnter={() => {brighten(3)()}}
+           onMouseLeave={() => darken(3)()}>Gabber Modus Operandi Galvanize Indonesian Folk Into Feverish Footwork</div>
+         </div>
+
+
+
+
+         {/* <div className = 'small-pic-item'>
          <div className = 'small-pic' style={{"background-image": 'url('+window.sidepic2+')'}}>
            </div>
            <div className = 'small-pic-text'>Gabe 'Nandez Finds a Home in Hip-Hop</div>
          </div>
-         <div className = 'small-pic-item'>
+         <div className = 'small-pic-item-3'>
          <div className = 'small-pic' style={{"background-image": 'url('+window.sidepic3+')'}}>
            </div>
            <div className = 'small-pic-text'>Gabber Modus Operandi Galvanize Indonesian Folk Into Feverish Footwork</div>
-         </div>
+         </div> */}
        </div>
        </div>
      )
