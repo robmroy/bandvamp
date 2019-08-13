77d8d402c6799c3fb4592f8441cba323a04ab9fe
robmroy <robmyroy@gmail.com>
2019-08-13 12:28:15 -0700

    Put pictures in the showcase

--- frontend/components/showcase.jsx
+++ frontend/components/showcase.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import lunasol from '../assets/images/luna_y_sol.png';
 class Showcase extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render(){
-    
-     return <img src={window.lunaURL}/> ;
+    return 	(
+    <div className = 'showcase-pics-container'>
+    <div className='showcase-large' >
+      <img className = 'showcase-large-pic' src = {window.shura}/>
+      <div className='showcase-pic-main-text'>On "forever," Shura Keeps Love Center Stage</div>
+
+      <a target="_blank" href="https://daily.bandcamp.com/2019/08/12/shura-forevher-interview/">
+      <div className='read-more-container'> 
+      <div className='read-more'>Read More</div>
+       <div className='read-more-external-link'> external link </div>
+       </div>
+       </a>
+
+       </div>
+       <div className = 'small-pics-container'>
+         <div className = 'small-pic-item'>
+         <div className = 'small-pic' style={{"background-image": 'url('+window.sidepic1+')'}}>
+           <div className = 'small-pic-text'>High Scores: Ben Prunty’s Moody, Trance-Inspired “Photographs” OST</div>
+           </div>
+         </div>
+         <div>
+         <img className = 'small-pic' src = {window.sidepic2}/>
+         </div>
+         <div>
+         <img className = 'small-pic' src = {window.sidepic3}/>
+         </div>
+       </div>
+       </div>
+     )
+
   }
 }
 
