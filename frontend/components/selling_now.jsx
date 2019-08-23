7741a30eb9016ee2d2089bca2c4f45642a849f9f
robmroy <robmyroy@gmail.com>
2019-08-23 10:34:43 -0700

    Add country flags to carousel

--- frontend/components/selling_now.jsx
+++ frontend/components/selling_now.jsx
@@ -4,8 +4,19 @@ class SellingNow extends React.Component {
 
       super(props);
       this.n = 10;
-      this.allAlbums = this.props.albums.slice(0,11).sort(
+      const codes = ['us', 'gb', 'ca', 'de', 'it', 'se', 'fr', 'es', 'jp', 'pt', 'ch']
+      this.countries = {us: 'United States', gb: 'United Kingdom',
+    ca: 'Canada', de: 'Germany', it: 'Italy', se: 'Sweden',
+    fr: 'France', es: 'Spain', jp: 'Japan', pt: 'Portugal', ch: 'Switzerland'}
+      this.allAlbums = this.props.albums.sort(
         () => Math.random() - .5);
+    this.allAlbums.forEach(album => {
+        let flagIdx = Math.floor(
+            Math.random()**3 * this.allAlbums.length
+        )%codes.length;
+        console.log(flagIdx);
+        album['country'] = codes[flagIdx];
+    });
         
         this.state = { idx: 0, width: 0}
         this.rotate = this.rotate.bind(this);
@@ -21,6 +32,14 @@ class SellingNow extends React.Component {
     componentDidMount(){
         this.rotate();
     }
+
+    linkToShow(album){
+        return () => {
+            this.props.history.push({pathname: `/band/${album.band_id}`,
+        state: {albumId: album.id}});}
+  
+    }
+      
 
     rotate(){
         let {idx} = this.state;
@@ -43,7 +62,6 @@ class SellingNow extends React.Component {
     }
 
     render(){
-        console.log(this.state.width);
             let allAlbums = this.allAlbums;
             const l = allAlbums.length;
             const idx = this.state.idx;
@@ -57,11 +75,16 @@ class SellingNow extends React.Component {
         return (
         <div className = "carousel">
             {albums.map((alb, i) => 
-                <div key={idx + i} className = {`carousel-img-wrapper${i<2 ? ` ${arr[i]}` : ''}`}
+                <div key={idx + i} className = {`carousel-item${i<2 ? ` ${arr[i]}` : ''}`}
                 ref={this.eight[i]} >
                     <img src={alb.photoUrl} className='carousel-img'/>
+                    <div style={{fontWeight: "bold"}}>{alb.name}</div>
+                    <div>by {alb.band.band_name}</div>
+                    {i > 0 ? <div className='ellipsis'> in <span className=
+                    {`flag flag-${alb.country}`}></span> 
+                    {` ${this.countries[alb.country]}`}
+                        </div>: null}
                     </div>)}
-            {/* <div ref ={this.testRef} className='carousel-img'>Test</div> */}
         </div>)
     }
 }
